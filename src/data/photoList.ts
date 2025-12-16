fdb128efffb156abf9c958e1828bee00a3463ca5
Carlos Alberto da Silva <slayer.br@gmail.com>
2025-12-16 15:58:24 -0300

    Fix: ajustado path do css

--- src/data/photoList.ts
+++ src/data/photoList.ts
@@ -1,12 +1,12 @@
 import { Photo } from '@/src/types/Photo';
 export const photoList: Photo[] = [
-  { id: 1, url: "/galeria-fotos/assets/1.jpg" },
-  { id: 2, url: "/galeria-fotos/assets/2.jpg" },
-  { id: 3, url: "/galeria-fotos/assets/3.jpg" },
-  { id: 4, url: "/galeria-fotos/assets/4.jpg" },
-  { id: 5, url: "/galeria-fotos/assets/5.jpg" },
-  { id: 6, url: "/galeria-fotos/assets/6.jpg" },
-  { id: 7, url: "/galeria-fotos/assets/7.jpg" },
-  { id: 8, url: "/galeria-fotos/assets/8.jpg" },
-  { id: 9, url: "/galeria-fotos/assets/9.jpg" },
+  { id: 1, url: "/assets/1.jpg" },
+  { id: 2, url: "/assets/2.jpg" },
+  { id: 3, url: "/assets/3.jpg" },
+  { id: 4, url: "/assets/4.jpg" },
+  { id: 5, url: "/assets/5.jpg" },
+  { id: 6, url: "/assets/6.jpg" },
+  { id: 7, url: "/assets/7.jpg" },
+  { id: 8, url: "/assets/8.jpg" },
+  { id: 9, url: "/assets/9.jpg" },
 ];
