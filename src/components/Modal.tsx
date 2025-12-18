6a083d933af88671c065e25054b6d63b6be587fc
Carlos Alberto da Silva <slayer.br@gmail.com>
2025-12-18 16:31:58 -0300

    Fix: ajustado path do css

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -9,44 +9,19 @@ export const Modal = ({ image, closeModal }: Props) => {
     <>
       <div
         onClick={closeModal}
-        style={{
-          position: 'fixed',
-          left: 0,
-          top: 0,
-          right: 0,
-          bottom: 0,
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: 'rgba(0, 0, 0, 0.9)',
-          zIndex: 50,
-        }}
+        className="fixed inset-0 flex justify-center items-center bg-black/90 z-50"
       >
-        <div style={{ position: 'relative', width: '100%', height: '100%' }}>
+        <div className="relative w-full h-full">
           <img
             src={image}
             alt="Imagem em tela cheia"
-            style={{
-              objectFit: 'contain',
-              width: '100%',
-              height: '100%',
-            }}
+            className="object-contain w-full h-full"
           />
         </div>
       </div>
       <div
         onClick={closeModal}
-        style={{
-          position: 'fixed',
-          top: '1.25rem',
-          right: '1.25rem',
-          width: '2.5rem',
-          height: '2.5rem',
-          cursor: 'pointer',
-          color: 'white',
-          fontSize: '3rem',
-          zIndex: 50,
-        }}
+        className="fixed top-5 right-5 w-10 h-10 cursor-pointer text-white text-5xl z-50"
       >
         x
       </div>
