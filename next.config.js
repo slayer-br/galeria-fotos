69138055bbefaa63841af11bc095a7dbf31ecbdc
Carlos Alberto da Silva <slayer.br@gmail.com>
2025-12-22 15:50:06 -0300

    Configura deploy para GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
+  output: 'export',
+  basePath: '/galeria-fotos',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
